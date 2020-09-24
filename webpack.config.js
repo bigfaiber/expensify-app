dc4c72fa4c79c7f315f49ce250dc051310ca87e7
bigfaiber <greatfaiber@gmail.com>
2020-09-24 16:28:15 -0500

    setup heroku production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = (env) => {
   const isProduction = env === 'production'
-
+  
   return {
     entry: './src/app.js',
   output: {
